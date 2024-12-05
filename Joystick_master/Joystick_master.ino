84ad39236e544b4333e1f3972f29d7ce79a6c3d4
DarkToino <167536866+DarkToino@users.noreply.github.com>
2024-12-05 02:08:53 +0000

    Update Joystick_master.ino

--- Joystick_master/Joystick_master.ino
+++ Joystick_master/Joystick_master.ino
@@ -1,49 +1,52 @@
-#include <BluetoothSerial.h>
+//#include <BluetoothSerial.h>
 
 #define pinYAxis 34
 #define pinXAxis 35
 
-BluetoothSerial SerialBT;
+//BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32_Master"); // Name of the master device
-  Serial.println("The device started, now you can pair it with Bluetooth!");
+  //Serial.begin(9600);
+  //SerialBT.begin("ESP32_Master"); // Name of the master device
+  //Serial.println("The device started, now you can pair it with Bluetooth!");
 
   pinMode(pinXAxis, INPUT);
   pinMode(pinYAxis, INPUT);
 }
 
 void loop() {
-  int xValue = analogRead(pinXAxis); 
-  int yValue = analogRead(pinYAxis); 
+  int yValue = analogRead(pinXAxis) / 4; 
+  int xValue = analogRead(pinYAxis) / 4; 
 
   Serial.println(xValue);
   Serial.println(yValue);
   delay(500);
 
   // Map joystick values to actions
-  if (yValue < 400) { // Joystick pushed up
-    Serial.println("BACKWARDS");
-    SerialBT.println("BACKWARDS");
-    delay(100);
-  } else if (yValue > 3600) { // Joystick pulled down
+  if (yValue < 100) {
     Serial.println("FORWARDS");
-    SerialBT.println("FORWARDS");
+    //SerialBT.println("FORWARDS");
+    delay(100);
+  }
+  if (yValue > 900) {
+    Serial.println("BACKWARDS");
+    //SerialBT.println("BACKWARDS");
     delay(100);
   }
 
-  if (xValue < 400) { // Joystick pushed left
-    Serial.println("LEFT");
-    SerialBT.println("LEFT");
-    delay(100);
-  } else if (xValue > 3600) { // Joystick pushed right
+  if (xValue < 100) { 
     Serial.println("RIGHT");
-    SerialBT.println("RIGHT");
+    //SerialBT.println("RIGHT");
+    delay(100);
+  }
+  if (xValue > 900) { 
+    Serial.println("LEFT");
+    //SerialBT.println("LEFT");
     delay(100);
   }
 
-  delay(100); // Delay for readability
+  delay(100);
 }
 
 
